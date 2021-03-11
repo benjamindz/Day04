209e4c3e138e91deb0d336b02ec68b8846c0e090
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-11 22:09:07 +0100

    Added string for my name, last name and hometown

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    var myFirstName = "Benjamin";
+    var myLastName = "Dzaferovic";
+    var myHomeTown = "Sarajevo";
+
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
+
